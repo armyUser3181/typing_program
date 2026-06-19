181c6037133b374212668d8901e621733dc665cb
armyUser3181 <yong3181.25.1@gmail.com>
2026-06-19 19:36:28 +0900

    시작

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const main = args => {
+
+}
+
+const init = args => {
+    
+}
+
+const createUI = args => {
+    const input = document.createElement('div');
+    const monitor = document.createElement('div');
+    const ground = document.createElement('div');
+    ground.classList.add('CenterElementClass')
+    document.body.appendChild(ground)
+    
+}
+
+createUI()

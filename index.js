1dcf5657bc2778c5f5ee1191b91f1d11988d724a
armyUser3181 <yong3181.25.1@gmail.com>
2026-06-20 23:14:04 +0900

    중도

--- index.js
+++ index.js
@@ -12,29 +12,34 @@ const init = args => {
 }
 
 const createUI = args => {
-    const input = document.createElement('div');
-    const monitor = document.createElement('div');
-    const ground = document.createElement('div');
-    ground.classList.add('CenterElementClass')
-    document.body.appendChild(ground)
-    dragEventElement({ground, move: ({pos})=>{
-        ground.style.left = pos.x;
-        ground.style.top = pos.y;
+    const input = createDivElement();
+    const monitor = createDivElement();
+    const ground = createDivElement();
+    document.body.appendChild(ground);
+    dragEventElement({element: ground, move: ({pos})=>{
+        ground.style.left = pos.x + 'px';
+        ground.style.top = pos.y + 'px';
+        //console.log(pos)
     }});
+
 }
 
 const dragEventElement = args => {
     const { element, down, up, move, eventElement = new EventElement } = args || {}
     const emitterIn = EventEmitter.form(element);
     const emitterOut = EventEmitter.form(document);
-    const rectOfElement = { x, y };
-    if( element instanceof HTMLElement && typeof down === 'function' && typeof up === 'function' && typeof move === 'function' && eventElement instanceof EventElement ) {
+    const rectOfElement = {};
+    rectOfElement.x = 0;
+    rectOfElement.y = 0;
+    if( element instanceof HTMLElement && eventElement instanceof EventElement ) {
         eventElement.push(
             new EventActionClass({
                 callback: ({event})=>{ if(event instanceof MouseEvent) {
                     const thisRect = element.getBoundingClientRect();
-                    rectOfElement.x = thisRect - event.clientX;
-                    rectOfElement.y = thisRect - event.clientY;
+                    const rect = {};
+                    rect.x = thisRect.x - event.clientX;
+                    rect.y = thisRect.y - event.clientY;
+                    for( const index of 'xy') rectOfElement[index] = rect[index];
                     return 'next';
                 }},
                 tag: 'mousedown',
@@ -42,9 +47,9 @@ const dragEventElement = args => {
             }),
             new EventActionClass({
                 callback: ({event})=>{ if(event instanceof MouseEvent) {
-                    const rect = { x, y };
-                    rect.x = event.clientX - rectOfElement.x;
-                    rect.y = event.clientY - rectOfElement.y;
+                    const rect = {};
+                    rect.x = event.clientX + rectOfElement.x;
+                    rect.y = event.clientY + rectOfElement.y;
                     const rt = move({pos: rect});
                     return 'loop';
                 }},
@@ -59,7 +64,15 @@ const dragEventElement = args => {
                 target: emitterOut,
             })
         )
+        eventElement.setup.call
+        emitterIn.bind
     }
+}
+
+const createDivElement = args => {
+    const div = document.createElement('div');
+    div.classList.add('CenterElementClass');
+    return div;
 }
 
 createUI()

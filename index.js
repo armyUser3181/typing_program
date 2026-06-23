c8054951c0d54397444cc054e9a87d10c3d73cec
armyUser3181 <yong3181.25.1@gmail.com>
2026-06-23 23:23:50 +0900

    저장

--- index.js
+++ index.js
@@ -8,20 +8,39 @@ const main = args => {
 }
 
 const init = args => {
-    
+    const nodes = createUI();
+    keyDown({element: nodes.input});
 }
 
 const createUI = args => {
-    const input = createDivElement();
-    const monitor = createDivElement();
-    const ground = createDivElement();
-    document.body.appendChild(ground);
+    const input = createInput();
+    const monitor = createMonitor();
+    const ground = createGround();
+    appBInd(ground);
+    appBInd(input, ground);
     dragEventElement({element: ground, move: ({pos})=>{
         ground.style.left = pos.x + 'px';
         ground.style.top = pos.y + 'px';
         //console.log(pos)
     }});
+    return { input, monitor, ground };
+}
 
+const keyDown = args => {
+    const { element, eventElement = new EventElement } = args || {};
+    const emitter = EventEmitter.form(document);
+    if( element instanceof HTMLDivElement && eventElement instanceof EventElement ) {
+        eventElement.push(new EventActionClass({ callback: ({event})=>{ if( event instanceof KeyboardEvent ) {
+            if( event.ctrlKey && event.key === 's' ) {
+                event.preventDefault();
+            }
+            element.textContent += event.key;
+            
+        }}, tag: 'keydown', target: emitter}));
+        eventElement.setup.classic;
+    }
+    emitter.bind
+    
 }
 
 const dragEventElement = args => {
@@ -71,10 +90,44 @@ const dragEventElement = args => {
     }
 }
 
+const createGround = args => {
+    const ground = createDivElement();
+    ground.style.width = '400px';
+    ground.style.height = '600px';
+    return ground;
+}
+
+const createInput = args => {
+    const input = createDivElement();
+    return input;
+}
+
+const createMonitor = args => {
+    const monitor = createDivElement();
+    return monitor;
+}
+
 const createDivElement = args => {
     const div = document.createElement('div');
     div.classList.add('CenterElementClass');
     return div;
 }
 
-createUI()
+const settingDivElement = args => {
+    const { div } = args;
+    if( div instanceof HTMLDivElement ) {
+        div.style.left;
+        const pr = div.parentElement.getBoundingClientRect();
+        const dr = div.getBoundingClientRect();
+        div.style.left = `${(pr.width - dr.width)/ 2}px`;
+    }
+    return div;
+}
+
+const appBInd = (element, parent = document.body) => {
+    parent.appendChild(element);
+    settingDivElement({div: element});
+    return element;
+}
+
+init();
